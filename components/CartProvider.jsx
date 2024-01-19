95718b733db7dc29912b5677afdc040ed19c016a
Devhell69 <paelalit@gmail.com>
2024-01-20 02:15:11 +0700

    update provider

--- components/CartProvider.jsx
+++ components/CartProvider.jsx
@@ -8,8 +8,8 @@ const CartProvider = ({ children }) => {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="https://next14-ecommerce-cms.vercel.app/stripe/success"
+      cancelUrl="https://next14-ecommerce-cms.vercel.app/stripe/error"
       language="en-US"
       billingAddressCollection={true}
       shouldPersist={true}
